d72eed3b8bd30483281b8ba59b8c02300ebd7ede
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-10-29 19:50:11 -0400

    Hello world in java

---
+++ scripts/hello_world_choyiny.java
@@ -0,0 +1,10 @@
+// LANGUAGE: Java
+// ENV: JVM
+// AUTHOR: Cho Yin Yong
+// GITHUB: https://github.com/choyiny
+
+public class HelloHacktoberfest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
